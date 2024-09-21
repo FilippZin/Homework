4ff1ec5849c3b6a24f9dfc5fb3e832e80f9a4657
FilippZin <filippwork777@gmail.com>
2024-09-21 11:07:13 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,9 @@
+i = 0
+my_list = [-42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+len_list = len(my_list)
+while i <= len_list :
+    if my_list[i] < 0:
+        break
+    else:
+        print(my_list[i])
+        i += 1
